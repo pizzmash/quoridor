6df4654627962955981c3abe086f160bdf70ee4f
Yuma Suzuki <s1813561@s.tsukuba.ac.jp>
2019-05-07 21:52:29 +0900

    壁がおける条件を修正

--- ditch.py
+++ ditch.py
@@ -9,11 +9,20 @@ class Ditch:
         self.vertical = [[self.STATE.EMPTY] * (size-1) for v in range(size)]
         self.xpt = [[self.STATE.EMPTY] * (size-1) for v in range(size-1)]
 
-    def is_fillable(self, h, v):
-        return self.xpt[v][h] == self.STATE.EMPTY
+    def is_fillable_horizontal(self, h, v):
+        is_empty_1 = self.horizontal[v][h] == self.STATE.EMPTY
+        is_empty_2 = self.horizontal[v][h+1] == self.STATE.EMPTY
+        is_empty_3 = self.xpt[v][h] == self.STATE.EMPTY
+        return is_empty_1 and is_empty_2 and is_empty_3
+
+    def is_fillable_vertical(self, h, v):
+        is_empty_1 = self.vertical[v][h] == self.STATE.EMPTY
+        is_empty_2 = self.vertical[v+1][h] == self.STATE.EMPTY
+        is_empty_3 = self.xpt[v][h] == self.STATE.EMPTY
+        return is_empty_1 and is_empty_2 and is_empty_3
 
     def fill_horizontal(self, h, v):
-        if self.is_fillable(h, v):
+        if self.is_fillable_horizontal(h, v):
             self.horizontal[v][h] = self.FILLED
             self.horizontal[v][h+1] = self.FILLED
             return True
@@ -21,7 +30,7 @@ class Ditch:
             return False
 
     def fill_vertical(self, h, v):
-        if self.is_fillable(h, v):
+        if self.is_fillable_vertical(h, v):
             self.vertical[v][h] = self.FILLED
             self.vertical[v+1][h] = self.FILLED
             return True
