3201c52e930dccf36692deec4860d434d89c0e45
Yuma Suzuki <s1813561@s.tsukuba.ac.jp>
2019-05-09 03:12:31 +0900

    名前の調整

--- human.py
+++ human.py
@@ -4,7 +4,7 @@ from move import PieceMove, HorizontalWallMove, VerticalWallMove
 
 class Human(Player):
     def think(self):
-        print('1: 動く\n2: 横の壁\n 3: 縦の壁')
+        print('1: 動く\n2: 横の壁\n 3:縦の壁')
         mode = int(input())
         print('どこに？')
         v, h = [int(i) for i in input().split()]
