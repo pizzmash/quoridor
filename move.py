16ea7604b09df82f1219ea4263f0152e02070df7
Yuma Suzuki <s1813561@s.tsukuba.ac.jp>
2019-05-09 00:29:47 +0900

    moveはプロパティにboardを持たないように変更

--- move.py
+++ move.py
@@ -1,42 +1,34 @@
-from enum import Enum
 from abc import ABCMeta, abstractmethod
 
 
 class Move(metaclass=ABCMeta):
-    def __init__(self, board, player):
-        self.board = board
-        self.player = player
-
     @abstractmethod
-    def launch(self):
+    def launch(self, board):
         raise NotImplementedError
 
 
 class PieceMove(Move):
-    def __init__(self, board, player, h, v):
-        super(PieceMove, self).__init__(board, player)
+    def __init__(self, h, v):
         self.h = h
         self.v = v
 
-    def launch(self):
-        return self.board.move_piece(self.h, self.v)
+    def launch(self, board):
+        return board.move_piece(self.h, self.v)
 
 
 class HorizontalWallMove(Move):
-    def __init__(self, board, player, h, v):
-        super(HorizontalWallMove, self).__init__(board, player)
+    def __init__(self, h, v):
         self.h = h
         self.v = v
 
-    def launch(self):
-        return self.board.put_horizontal_wall(self.h, self.v)
+    def launch(self, board):
+        return board.put_horizontal_wall(self.h, self.v)
 
 
 class VerticalWallMove(Move):
-    def __init__(self, board, player, h, v):
-        super(VerticalWallMove, self).__init__(board, player)
+    def __init__(self, h, v):
         self.h = h
         self.v = v
 
-    def launch(self):
-        return self.board.put_vertical_wall(self.h, self.v)
+    def launch(self, board):
+        return board.put_vertical_wall(self.h, self.v)
