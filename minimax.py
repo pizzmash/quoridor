de78c8c33b2a3e6ce3e74ff74b90a38ed207c4b9
Yuma Suzuki <s1813561@s.tsukuba.ac.jp>
2019-05-09 23:48:14 +0900

    MiniMaxクラスを作成

---
+++ minimax.py
@@ -0,0 +1,10 @@
+from human import Human
+
+
+class MiniMax(Human):
+    def __init__(self, eval, depth=3):
+        self.eval = eval
+        self.depth = depth
+
+    def think(board):
+        pass
