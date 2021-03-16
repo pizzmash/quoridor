3c383af8319264ed3bce4a0d02c3f5255e342d57
pizzmash <music_xxx_sfy@yahoo.co.jp>
2021-03-16 22:34:03 +0900

    randombot改良

--- randombot.py
+++ randombot.py
@@ -1,9 +1,23 @@
 import random
 
 from player import Player
+from move import PieceMove
 
 
 class RandomBot(Player):
+    def __init__(self, all_random=False):
+        self.all_random = all_random
+
     def think(self, board):
         moves = list(board.regal_move())
+        if not self.all_random:
+            move_type = random.randrange(2)
+            if move_type == 0:
+                moves = [move for move in moves if isinstance(move, PieceMove)]
+            else:
+                moves_ = [move for move in moves if not isinstance(move, PieceMove)]
+                if len(moves_) == 0:
+                    moves = [move for move in moves if isinstance(move, PieceMove)]
+                else:
+                    moves = moves_
         return random.choice(moves)
